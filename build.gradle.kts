316039dbe5036ae27bfa50c72fb50e6daecdf1ba
Rémi Bleuse <remi.bleuse@gmail.com>
2022-05-04 10:55:57 +0900

    fix startup error

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
--- src/main/kotlin/org/rbleuse/inbox/configuration/DataStaxAstraProperties.kt
+++ src/main/kotlin/org/rbleuse/inbox/configuration/DataStaxAstraProperties.kt
@@ -1,10 +1,8 @@
 package org.rbleuse.inbox.configuration
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 import java.io.File
 
-@ConstructorBinding
 @ConfigurationProperties("datastax.astra")
 data class DataStaxAstraProperties(
     val secureConnectBundle: File
