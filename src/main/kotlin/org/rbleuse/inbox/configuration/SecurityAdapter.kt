8fcd160f8187be082c4b224eba258cc39949053a
Rémi Bleuse <remi.bleuse@gmail.com>
2023-02-25 18:45:28 +0900

    fix security

--- src/main/kotlin/org/rbleuse/inbox/configuration/SecurityAdapter.kt
+++ src/main/kotlin/org/rbleuse/inbox/configuration/SecurityAdapter.kt
@@ -1,21 +1,38 @@
 package org.rbleuse.inbox.configuration
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 
 @Configuration
-class SecurityAdapter : WebSecurityConfigurerAdapter() {
+class SecurityAdapter {
 
-    @Throws(Exception::class)
-    override fun configure(http: HttpSecurity) {
-        http.authorizeRequests().antMatchers("/", "/error").permitAll().anyRequest().authenticated()
-        http.exceptionHandling().authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-        http.logout().logoutSuccessUrl("/").permitAll()
-        http.oauth2Login()
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeHttpRequests {
+                authorize("/", permitAll)
+                authorize("/error", permitAll)
+                authorize(anyRequest, authenticated)
+            }
+            exceptionHandling {
+                authenticationEntryPoint = HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+            }
+            csrf {
+                csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse()
+            }
+            logout {
+                logoutSuccessUrl = "/"
+                permitAll
+            }
+            oauth2Login { }
+        }
+
+        return http.build()
     }
 }
