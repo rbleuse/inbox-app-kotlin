e6e9be39cb035e914af997d0aa801ab20e7195f4
Rémi Bleuse <remi.bleuse@gmail.com>
2024-02-09 15:13:01 +0900

    fix keyspace creation

---
+++ src/main/kotlin/org/rbleuse/inbox/configuration/CassandraConfiguration.kt
@@ -0,0 +1,24 @@
+package org.rbleuse.inbox.configuration
+
+import com.datastax.oss.driver.api.core.CqlSession
+import com.datastax.oss.driver.api.core.CqlSessionBuilder
+import org.springframework.boot.autoconfigure.cassandra.CassandraProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.cassandra.core.cql.generator.CreateKeyspaceCqlGenerator
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification
+
+@Configuration
+class CassandraConfiguration {
+
+    @Bean
+    fun cqlSession(builder: CqlSessionBuilder, properties: CassandraProperties): CqlSession {
+        // This creates the keyspace on startup
+        builder.withKeyspace("system").build().use { session ->
+            session.execute(CreateKeyspaceCqlGenerator
+                .toCql(CreateKeyspaceSpecification.createKeyspace(properties.keyspaceName).ifNotExists()))
+        }
+
+        return builder.withKeyspace(properties.keyspaceName).build()
+    }
+}
