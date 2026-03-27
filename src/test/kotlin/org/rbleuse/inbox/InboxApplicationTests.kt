44d02aa3da6fe680c4caf2b21a60d3654916ddf2
Rémi Bleuse <remi.bleuse@gmail.com>
2026-03-27 10:32:36 +0900

    Update Cassandra container version to 5.0.7

--- src/test/kotlin/org/rbleuse/inbox/InboxApplicationTests.kt
+++ src/test/kotlin/org/rbleuse/inbox/InboxApplicationTests.kt
@@ -13,7 +13,7 @@ class InboxApplicationTests {
     companion object {
         @Container
         @ServiceConnection
-        val cassandraContainer: CassandraContainer = CassandraContainer("cassandra:5.0.4")
+        val cassandraContainer: CassandraContainer = CassandraContainer("cassandra:5.0.7")
     }
 
     @Test
